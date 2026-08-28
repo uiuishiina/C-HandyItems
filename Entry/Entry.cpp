3e10eb281516f6463304cb6d224dd628cd887cc5
uiuishiina <shiina04jbs10@gmail.com>
2026-08-28 16:19:37 +0900

    今回作成したもの
    Unorderedmapを扱う汎用マップ
    DirectXmathをラッパーするクラス
    循環参照を行ったりするのに必要なCirclurIndex
    コピー&ムーブ不可クラス

--- Entry/Entry.cpp
+++ Entry/Entry.cpp
@@ -1,6 +1,15 @@
-#include"../ItemsHeader/Others/EnumToIndex.h"
+#include<iostream>
+#include"../ItemsHeader/Container/UnorderedMapBase.h"
+
+using namespace std;
 
 int main() {
+
+	container::UnorderedMapBase<int, bool> con{};
+	con.add_value(1, true);
+	if (con.get_value(1).value()) {
+		cout << "Hello HandyItems" << endl;
+	}
 
 	return 0;
 }
