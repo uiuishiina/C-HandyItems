b2a4af134a342abb13af9fae7f9599dd08a1e08c
うい 椎名 <shiina04jbs10@outlook.com>
2026-09-16 19:07:35 +0900

    Matrix仮完成
    この後Functionをどうするかや、Includeの依存関係を見直したり

--- Entry/Entry.cpp
+++ Entry/Entry.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include"../ItemsHeader/Math/BaseStruct/Transform.h"
+#include"../ItemsHeader/Math/Function/MathFQuaternion.h"
+#include"../ItemsHeader/Math/Function/MathFMatrix.h"
 #include <iomanip>
 
 using namespace std;
@@ -9,16 +11,31 @@ int main() {
 
 	
 	Transform trans{};
-	trans.translation_ = { 10,0,0 };
+	trans.translate({ 10,0,0 });
+	trans.rotate({ 0,MathF::HALF_PI,0 });
+	trans.scale_by({ 2,3,4 });
 
-	const auto mat = trans.get_matrix();
+	auto mat = trans.get_matrix();
 
-	const auto pos = MathF::translation_from_matrix(mat);
+	cout << "mat[0].x_ = " << mat[0].x_ << endl;
+	cout << "mat[0].y_ = " << mat[0].y_ << endl;
+	cout << "mat[0].z_ = " << mat[0].z_ << endl;
+	cout << "mat[0].w_ = " << mat[0].w_ << endl << endl;
 
-	cout << "pos.x_ = " << pos.x_ << endl;
-	cout << "pos.y_ = " << pos.y_ << endl;
-	cout << "pos.z_ = " << pos.z_ << endl;
+	cout << "mat[1].x_ = " << mat[1].x_ << endl;
+	cout << "mat[1].y_ = " << mat[1].y_ << endl;
+	cout << "mat[1].z_ = " << mat[1].z_ << endl;
+	cout << "mat[1].w_ = " << mat[1].w_ << endl << endl;
+
+	cout << "mat[2].x_ = " << mat[2].x_ << endl;
+	cout << "mat[2].y_ = " << mat[2].y_ << endl;
+	cout << "mat[2].z_ = " << mat[2].z_ << endl;
+	cout << "mat[2].w_ = " << mat[2].w_ << endl << endl; 
 	
+	cout << "mat[3].x_ = " << mat[3].x_ << endl;
+	cout << "mat[3].y_ = " << mat[3].y_ << endl;
+	cout << "mat[3].z_ = " << mat[3].z_ << endl;
+	cout << "mat[3].w_ = " << mat[3].w_ << endl << endl;
 	
 	return 0;
 }
--- ItemsHeader/Math/BaseStruct/Transform.h
+++ ItemsHeader/Math/BaseStruct/Transform.h
@@ -1,4 +1,5 @@
 #pragma once
+#include"../Function/MathFQuaternion.h"
 #include"../Function/MathFMatrix.h"
 
 /// <summary>
@@ -31,7 +32,12 @@ namespace HandyItem {
 			/// <summary>
 			/// 拡大率
 			/// </summary>
-			Float3 scale_{};
+			Float3 scale_{ 1,1,1 };
+
+			/// <summary>
+			/// 親トランスフォーム構造体
+			/// </summary>
+			Transform* parent_{};
 
 			/* ========== メンバー関数 ========== */
 
@@ -46,7 +52,8 @@ namespace HandyItem {
 			/// <param name="translate">平行移動量</param>
 			/// <param name="rotate">回転量</param>
 			/// <param name="scale">拡大率</param>
-			Transform(const Float3& translate,
+			Transform(
+				const Float3& translate,
 				const Quaternion& rotate,
 				const Float3& scale
 			) :
@@ -61,18 +68,65 @@ namespace HandyItem {
 			/// <returns>取得した行列</returns>
 			[[nodiscard]] Matrix get_matrix() const {
 
-				return MathF::transform_to_matrix(
+				const auto mat = MathF::transform_to_matrix(
 					translation_,
 					rotation_,
 					scale_
 				);
+
+				return parent_ ? parent_->get_matrix() * mat : mat;
 			}
 
-			void translate(const Float3& move) {
+			/// <summary>
+			/// 平行移動適用関数
+			/// </summary>
+			/// <param name="move">適用する移動ベクトル</param>
+			void translate(
+				const Float3& move
+			) {
 
-				MathF::apply_translation()
+				translation_ += move;
+			}
+			
+			/// <summary>
+			/// 回転適用関数
+			/// </summary>
+			/// <param name="rotation">適用するクオータニオン</param>
+			void rotate(
+				const Quaternion& delta
+			) {
+
+				rotation_ = (
+					rotation_ * delta
+					).normalized();
 			}
 
+			/// <summary>
+			/// 回転適用関数
+			/// </summary>
+			/// <param name="euler">適用するオイラー角</param>
+			void rotate(
+				const Float3& euler
+			) {
+
+				rotate(
+					MathF::from_euler(euler)
+				);
+			}
+
+			/// <summary>
+			/// 拡大率適用関数
+			/// </summary>
+			/// <param name="scale">適用する拡大率</param>
+			void scale_by(
+				const Float3& scale
+			) {
+
+				scale_.x_ *= scale.x_;
+				scale_.y_ *= scale.y_;
+				scale_.z_ *= scale.z_;
+			}
+		
 		};
 	}
 }
